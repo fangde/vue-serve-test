cc751edcdfa2517c83d8ab64241b21f8afc0cf3d
Fangde Liu <hibana1983@gmail.com>
2018-10-14 12:49:03 +0800

    add data

--- App.vue
+++ App.vue
@@ -7,12 +7,11 @@
         <h2>
             {{ baidu }}
         </h2>
+       
+
+        <img ref='source_img' src="im1.jpg" alt="Trulli" width="500" height="333">
         <canvas ref='image'>
-
-        
         </canvas>
-
-        <img ref='source_img' src="pic_trulli.jpg" alt="Trulli" width="500" height="333">
 
          <button ref='testbtn' @click='showImage'>ImageShow</button>
     </div>
@@ -22,44 +21,40 @@
 <script>
 /* eslint-disable no-alert, no-console */
 
-import test from './service/test'
-import {test2} from './service/test'
+import test from "./service/test";
+import { test2 } from "./service/test";
 export default {
-
-    data(){
-
-        
-       return { 
-           what:  test('what'),
-           baidu: test2('hello'),
-           images: ["http://pic1.win4000.com/pic/c/0a/14c4480667.jpg",
-            'http://img05.tooopen.com/images/20150330/tooopen_sy_83867387932.jpg'
-
-           ]
-          
-       };
+  data() {
+    return {
+      what: test("what"),
+      baidu: test2("hello"),
+      images: ["im1.jpg", "im2.jpg", "im3.jpg"],
+      sequence: []
+    };
+  },
+  methods: {
+    showImage() {
+      console.log("clicked");
+      console.log(this.$refs.testbtn);
+      this.$refs.testbtn.innerText = "waht";
+      console.log(this.$refs.image);
+      this.$refs.source_img.src = "./im1.jpg";
     },
-    methods:{
-        showImage(){
-            
-    
-            console.log('clicked')
-            console.log(this.$refs.testbtn)
-            this.$refs.testbtn.innerText="waht"
-            console.log(this.$refs.image)
-            this.$refs.source_img.src="pic_trulli.jpg"
-
-        }
+    loadImage() {
+      function load(counter) {
+        let p = new Image();
+        p.src = this.images[counter];
+        p.onload();
+      }
     }
-    
-}
+  }
+};
 </script>
 
 <style scoped>
 canvas {
-    width: 800px;
-    height:600px;
-
+  width: 800px;
+  height: 600px;
 }
 </style>
 
---
+++ testLooper.vue
@@ -0,0 +1,19 @@
+<template>
+	<vue-perfectlooper v-bind="loop"></vue-perfectlooper>
+</template>
+<script>
+import VuePerfectlooper from "vue-perfectlooper";
+
+export default {
+  components: { VuePerfectlooper },
+  data: function() {
+    return {
+      loop: {
+        id: "echo",
+        poster: "./0010.png",
+        src: ["./0010.png", "./0011.png", "./0013.png"]
+      }
+    };
+  }
+};
+</script>
